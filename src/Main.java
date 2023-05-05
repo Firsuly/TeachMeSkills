6c72144367f229d2d02eabedda732104b872931b
TeachMySkill <pasha27091983@gmail.com>
2023-05-05 22:44:40 +0300

    update HomeWork2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,30 @@
-
-public class Main
+//This is an addition HomeWork2
+import java.util.Scanner;
+class Main
 {
-    public static void main(String[] args)
-    {
+    // A field for playing tic tac toe
+    public static void main(String[] args) {
+        System.out.println("    |    |    ");
+        System.out.println("____|____|____");
+        System.out.println("    |    |    ");
+        System.out.println("____|____|____");
+        System.out.println("    |    |    ");
+        System.out.println("    |    |    ");
 
-        System.out.println("Hello and welcome!");
+        {
+            //Some kind of Christmas tree
 
+            System.out.println("    *    ");
+            System.out.println("   ***   ");
+            System.out.println("  *****  ");
+            System.out.println(" ******* ");
+            System.out.println("*********");
+        }
+
+        {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите хоть что-нибудь!");
+            String s = scanner.nextLine();
+            System.out.println("I am study in the school TeachMeSkills");
         }
     }
