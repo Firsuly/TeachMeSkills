ae6687d09f9a33fa0414cf14b981761bbaca4a95
TeachMySkill <pasha27091983@gmail.com>
2023-05-09 23:04:33 +0300

    update HomeWork3

--- src/Main.java
+++ src/Main.java
@@ -3,53 +3,70 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) { // Функции пульта управления
 
-        getScanner();
-        getVariables();
+        //getPersonalData();
+        //convertToChar();
         getMassIndex();
     }
 
-    public static void getScanner() {
+    public static void getPersonalData()
+    {
         Scanner enter = new Scanner(System.in);
 
         System.out.print("Please enter you name: ");
         String name = enter.nextLine();
-        System.out.println("You name is: " + "Pavel");
+
         System.out.print("Please enter you surname: ");
         String surname = enter.nextLine();
-        System.out.println("You surname is: " + "Firsov");
-        System.out.print("Please enter you age: ");
-        String age = enter.nextLine();
-        System.out.println("You age is: " + "37");
-        System.out.print("Please enter you height: ");
-        String height = enter.nextLine();
-        System.out.println("You height is: " + "180");
-        System.out.print("Please enter you weight: ");
-        String weight = enter.nextLine();
-        System.out.println("You weight is: " + "79");
 
-        enter.close(); // закрыл ресурс
+        System.out.print("Please enter you age: ");
+        int age = enter.nextInt();
+
+        System.out.print("Please enter you height: ");
+        int height = enter.nextInt();
+
+        System.out.print("Please enter you weight: ");
+        Double weight = enter.nextDouble();
+
+
+        System.out.println("You name is: " + name);
+        System.out.println("You surname is: " + surname);
+        System.out.println("You age is: " + age);
+        System.out.println("You height is: " + height);
+        System.out.println("You weight is: " + weight);
+
+
     }
 
-    public static void getVariables()
+    public static void convertToChar()
     {
-        char mySymbol = 'C';
-        char mySymbola = 'a';
-        System.out.println("My letter C - " + (int)mySymbol);
-        System.out.println("My letter a - " + (int)mySymbola);
+        Scanner convert = new Scanner(System.in);
+
+        System.out.print("Please enter you symbol: ");
+        char symbol = (char) convert.nextInt();
+        System.out.println("You have entered the symbol: " + symbol);
+        System.out.println("This symbol numeric is: " + (int) symbol);
+
     }
 
     public static void getMassIndex()
     {
+    Scanner BMI = new Scanner(System.in);
+
         System.out.println("BMI we count by the formula BMI = kg/m2 ");
 
-        /* Я конечно написал эту формулу согласно заданию, но судя по всему она не правильная,
-        нашел другую формулу в инете и рассчет сделал по ней*/
+        System.out.print("Please enter you weight: ");
+        double weight = BMI.nextDouble();
 
-       float weight = 79;
-       float hight = 1.8F;
+        System.out.print("Please enter you height: ");
+        int height = BMI.nextInt();
 
-        System.out.println(weight/(hight + hight));
-        System.out.println("This is you Body Mass Index - are you okay!");
+        System.out.print("You BodyMassIndex is: " + weight /(height * height / 10000));
+        double formula = BMI.nextDouble();
+
+      BMI.close(); //закрыл ресурс
+
+        // вроде все ошибки исправил
+
 
     }
 }
