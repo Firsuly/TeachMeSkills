c4e1598c6d53a7ddbca5646de71c1d7b4d63b599
TeachMySkill <pasha27091983@gmail.com>
2023-05-12 00:35:22 +0300

    one HomeWork4

--- src/Main.java
+++ src/Main.java
@@ -17,14 +17,17 @@ class Main {
         switch (month) {
             case 12: case 1: case 2:
                 System.out.println("winter");
+                break;
 
-                break; case 3: case 4: case 5:
+                case 3: case 4: case 5:
                 System.out.println("spring");
+                break;
 
-                break; case 6: case 7: case 8:
+                case 6: case 7: case 8:
                 System.out.println("summer");
+                break;
 
-                break; case 9: case 10: case 11:
+                case 9: case 10: case 11:
                 System.out.println("autumn");
                 break;
             default:
